9f0735ae4936ae91b3af1b263264c5ab603afbe0
U-BSD\405251 <405251@SHS-10D9C063E.beaverton.k12.or.us>
2022-02-14 12:06:08 -0800

    created node class for binary tree

---
+++ binarynode.h
@@ -0,0 +1,13 @@
+/*
+ * Class for a node of a binary tree. Contains data, a left pointer and a right pointer. 
+ * Author: Max Shi
+ * Last Modified: 2/14/2022
+ */
+
+class bnode
+{
+ public:
+  char data;
+  bnode* left;
+  bonde* right;
+};
